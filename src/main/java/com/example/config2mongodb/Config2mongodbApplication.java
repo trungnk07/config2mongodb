47f5ca6e6d29afca9f4824c4cb4f08d6a934066c
trungnk <nguyentrunghcmup@gmail.com>
2023-11-18 23:37:23 +0700

    add comment

--- src/main/java/com/example/config2mongodb/Config2mongodbApplication.java
+++ src/main/java/com/example/config2mongodb/Config2mongodbApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Config2mongodbApplication {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Config2mongodbApplication.class, args);
 	}
